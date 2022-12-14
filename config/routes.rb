e6c5b489477965374083a1bec3aedb340fe0ad59
adrien schaffner <adrien.schaffner@gmail.com>
2022-12-14 15:40:32 +0100

    creation of articles model & routes & controller & views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
 end
