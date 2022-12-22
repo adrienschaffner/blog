5eb122d764ebb116d1f0d3632d3d2d647b267e18
adrien schaffner <adrien.schaffner@gmail.com>
2022-12-22 12:52:46 +0100

    home page responsive with btn

--- app/javascript/controllers/dropdown_menu_controller.js
+++ app/javascript/controllers/dropdown_menu_controller.js
@@ -1,15 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets=["fa-bars", "nav-links"]
+  static targets=["fa-bars", "nav-links", "big-text"]
 
   connect() {
     console.log("Hello from dropdown menu controller!")
     const menuHamburger = document.querySelector(".fa-bars");
     const navLinks = document.querySelector(".nav-links");
+    const bigText = document.querySelector(".big-text");
 
     menuHamburger.addEventListener('click',()=>{
       navLinks.classList.toggle('mobile-menu');
+      bigText.classList.toggle('mobile-menu');
     })
   }
 }
