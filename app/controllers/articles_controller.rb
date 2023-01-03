d5ecc78ae5de1ca7ec585f32b5ad10728f2246da
adrien schaffner <adrien.schaffner@gmail.com>
2023-01-03 18:59:55 +0100

    articles rails return

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,11 +27,13 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
+      flash[:notice] = "Article was successfully created"
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
+
 
   def edit
   end
