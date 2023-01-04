320484d16d131117002e0733f0ed77a44d5de223
adrien schaffner <adrien.schaffner@gmail.com>
2023-01-04 19:36:51 +0100

    devise, not clear

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+ # frozen_string_literal: true
 
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
@@ -6,6 +6,23 @@
 # breaking changes in upgrades (i.e., in the event that future versions of
 # Devise change the default values for those options).
 #
+
+class TurboFailureApp < Devise::FailureApp
+
+  def respond
+    if request_format == :turbo_stream
+      :redirect
+    else
+      super
+    end
+  end
+
+  def skip_format
+    %w[html turbo_stream].include? request_format.to_s
+  end
+end
+
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -18,7 +35,11 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  # config.parent_controller = 'DeviseController'
+  config.parent_controller = 'TurboDeviseController'
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { sessions: 'admins/sessions'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+
   resources :articles do
     collection do
       get :top
@@ -17,3 +18,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# Ensure you have overridden routes for generated controllers in your routes.rb.
+# For example:
+# Rails.application.routes.draw do
+#   devise_for :users, controllers: {
+#     sessions: 'users/sessions'
+#   }
+# end
