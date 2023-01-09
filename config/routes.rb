5b63db9426dc600b52dccd26616184eafeedefa0
adrien schaffner <adrien.schaffner@gmail.com>
2023-01-09 14:44:56 +0100

    home page css responsive & project page first step & show article

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "project", to: "pages#project"
 
 
   resources :articles do
