b7f505b428a961be4e32a4e6e298ed28ae2abbe9
adrien schaffner <adrien.schaffner@gmail.com>
2023-01-26 13:12:47 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@
 require 'faker'
 
 Article.destroy_all
-40.times do |index|
-  Article.create!(title: Faker::Movies::Ghostbusters.character,
-    body: Faker::Movies::BackToTheFuture.quote,
-    status: "public",
-    rating: 5,
-    author: Faker::Movies::BackToTheFuture.character,
-    # picture: Faker::Avatar.image,
-  )
-end
+# 40.times do |index|
+#   Article.create!(title: Faker::Movies::Ghostbusters.character,
+#     body: Faker::Movies::BackToTheFuture.quote,
+#     status: "public",
+#     rating: 5,
+#     author: Faker::Movies::BackToTheFuture.character,
+#     # picture: Faker::Avatar.image,
+#   )
+# end
 
 p "Created #{Article.count} articles"
 
