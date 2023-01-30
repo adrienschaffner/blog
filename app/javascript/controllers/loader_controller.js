fbc33adab671097d2570c3c6563f2f6f1b3d1cd9
adrien schaffner <adrien.schaffner@gmail.com>
2023-01-31 00:01:21 +0100

    about responsive to delete

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -5,22 +5,24 @@ export default class extends Controller {
 
   connect() {
     console.log("hello from loader controller ! ");
-    const loader = document.querySelector('.loader');
-    const slider = document.querySelector('.slider');
-    const slogan = document.querySelector('.index-slogan');
-    const title = document.querySelector('.big-text');
+    // const loader = document.querySelector('.loader');
+    // const slider = document.querySelector('.slider');
+    // const slogan = document.querySelector('.index-slogan');
+    // const indexTitle = document.querySelector('.index-title');
+    // const title = document.querySelector('.big-text');
     // const slogan = document.querySelector('.font-slogan');
-    const btn = document.querySelector('.btn-list');
+    // const btn = document.querySelector('.btn-list');
     // const btn_login = document.querySelector('.btn-login');
     // Event.preventDefaulft()
-    var delayInMilliseconds = 2000; // second
-    var delayInMilliseconds1100 = 1100;
-    var delayInMilliseconds1400 = 1400;
+    // var delayInMilliseconds = 2000; // second
+    // var delayInMilliseconds1100 = 1100;
+    // var delayInMilliseconds1400 = 1400;
     // var delayInMilliseconds2900 = 2900;
 
     const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
     tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+    tl.fromTo(".index-title", { opacity: 0 }, { opacity: 1, duration: 0.8 });
     tl.fromTo(".index-slogan", { opacity: 0 }, { opacity: 1, duration: 1 });
     tl.to(".slider", { y: "-100%", duration: 1.4, delay: 0.5 });
     tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
